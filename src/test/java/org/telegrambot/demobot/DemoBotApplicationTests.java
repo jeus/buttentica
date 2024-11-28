ef14e29b42d433a64afa10357773375edbda4018
Jeus <alkhandani@gmail.com>
2024-11-28 01:40:22 +0100

    fixed some issue on build by maven

--- src/test/java/org/telegrambot/demobot/DemoBotApplicationTests.java
+++ src/test/java/org/telegrambot/demobot/DemoBotApplicationTests.java
@@ -3,7 +3,7 @@ package org.telegrambot.demobot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DemoBotApplicationTests {
 
     @Test
